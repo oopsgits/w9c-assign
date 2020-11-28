b821936efb15d752bbc20c7d5aef13606d2bf977
Ngure Njoroge <ngurenjoroge@ngures-MacBook-Air.local>
2020-11-28 00:24:33 -0700

    finished-w9e fillter function

--- js/app.js
+++ js/app.js
@@ -30,3 +30,87 @@ for(var index = 0; index < tweets.length; index++) {
     console.log("-----------------------");
 }
 
+
+
+var tweets = [
+    {
+        tweet: "what ever",
+        username: "jill",
+        created_at: "02-04-2020",
+        age: 11
+    },
+    {
+        tweet: "this is my first ever tweet",
+        username: "jimmy",
+        created_at: "02-04-2020",
+        age: 16
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "tommy",
+        created_at: "02-04-2020",
+        age: 90
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "tommy",
+        created_at: "02-09-2020",
+        age: 90
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "Ngure",
+        created_at: "02-04-2020",
+        age: 26
+    },
+    {
+        tweet: "uuuuurrrp",
+        username: "danny",
+        created_at: "06-11-2020",
+        age: 33
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "tommy",
+        created_at: "02-04-2020",
+        age: 90
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "jill",
+        created_at: "02-04-2020",
+        age: 11
+    },
+    {
+        tweet: "I can see this turing into my whole life :)",
+        username: "ngure",
+        created_at: "03-01-2019",
+        age: 26
+    },
+    {
+        tweet: " :):):):):):):):)",
+        username: "jack",
+        created_at: "02-04-2020",
+        age: 12
+    },
+
+
+];
+
+
+ function ageChecker(tweet) {
+    return tweet.age >= 18;
+    // if (tweet.age >= 18)
+    // return true;
+    // else return (console.log ,false)
+}
+
+      
+    
+    
+    
+    
+
+
+var overEightteenTweets = tweets.filter(ageChecker);
+console.log(overEightteenTweets);
